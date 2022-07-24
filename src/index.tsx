90e9eb3679a98670d2812db41683650c091f6b08
karolinasmile@mail.ru <karolinasmile@mail.ru>
2022-07-24 16:50:17 +0300

    less6

--- src/index.tsx
+++ src/index.tsx
@@ -10,3 +10,4 @@ ReactDOM.render(<App/>,  document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+//git remote set-url origin https://github.com/KarolinaEsepenok/todolistTwo.git
