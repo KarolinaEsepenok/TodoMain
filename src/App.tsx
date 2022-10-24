0eff9b8296a10273fd9992c1b4818d871fd63a3c
karolinasmile@mail.ru <karolinasmile@mail.ru>
2022-10-24 18:37:52 +0300

    lesson10Commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import {TaskType, Todolist} from './Todolist';
 import {v1} from 'uuid';
 import {AddItemForm} from './AddItemForm';
 import {AppBar, Button, Container, Grid, IconButton, Paper, Toolbar, Typography} from "@mui/material";
-import {Menu} from "@mui/icons-material";
+import {Menu} from '@mui/icons-material';
 
 
 export type FilterValuesType = "all" | "active" | "completed";
