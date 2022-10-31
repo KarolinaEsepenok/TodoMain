5524cdd8f41d79c209be7f02eacb15b836e7d29b
karolinasmile@mail.ru <karolinasmile@mail.ru>
2022-10-31 15:46:14 +0300

    12lesson

--- src/stories/Task.stories.tsx
+++ src/stories/Task.stories.tsx
@@ -5,13 +5,19 @@ import { Button } from './Button';
 import {AddItemForm} from "../AddItemForm";
 import {action} from "@storybook/addon-actions";
 import {Task} from "../Task";
+import {TaskType} from "../Todolist";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'TODOLISTS/Task',
-  component: Task
+  component: Task,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-
+args:{
+  changeTaskStatus: action('changeTaskStatus'),
+  changeTaskTitle: action('changeTaskTitle'),
+  removeTask:action('removeTask'),
+  todolistId: '1'
+}
 } as ComponentMeta<typeof Task>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
@@ -20,7 +26,11 @@ const Template: ComponentStory<typeof Task> = (args) => <Task {...args} />;
 export const TaskIsDone = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 TaskIsDone.args = {
- addItem: action('Clicked button')
+    task: {id:'ggg',isDone:true, title:'JS'},
+
 };
 export const TaskIsNotDone = Template.bind({});
+TaskIsNotDone.args = {
+  task: {id:'ggggggg',isDone:false, title:'CSS'},
 
+};
