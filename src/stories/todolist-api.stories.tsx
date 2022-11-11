ef93298e236eced5ef2287971a7143a34aaf3a65
karolinasmile@mail.ru <karolinasmile@mail.ru>
2022-11-11 17:30:13 +0300

    13lesson

--- src/stories/todolist-api.stories.tsx
+++ src/stories/todolist-api.stories.tsx
@@ -1,15 +1,29 @@
 
 import React, {useEffect, useState} from 'react'
+import axios from 'axios'
 
 export default {
     title: 'API'
 }
 //abd96315-58cc-42a7-a904-226e13c834ce
+const settings={
+    withCredentials:true,
+    headers:{
+        "API-KEY": "abd96315-58cc-42a7-a904-226e13c834ce"
+    }
+}
+
+
 export const GetTodolists = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
         // здесь мы будем делать запрос и ответ закидывать в стейт.
         // который в виде строки будем отображать в div-ке
+      axios.get("https://social-network.samuraijs.com/api/1.1/todo-lists", settings)
+        .then((res)=>{
+            setState(res.data)
+        })
+
 
     }, [])
     return <div>{JSON.stringify(state)}</div>
@@ -17,6 +31,11 @@ export const GetTodolists = () => {
 export const CreateTodolist = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
+        axios.post("https://social-network.samuraijs.com/api/1.1/todo-lists",{ title: "karolina"}, settings)
+            .then((res)=>{
+
+                setState(res.data)
+            })
     }, [])
 
     return <div>{JSON.stringify(state)}</div>
@@ -24,6 +43,11 @@ export const CreateTodolist = () => {
 export const DeleteTodolist = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
+        axios.delete("https://social-network.samuraijs.com/api/1.1/todo-lists/abd96315-58cc-42a7-a904-226e13c834ce", settings)
+            .then((res)=>{
+
+                setState(res.data)
+            })
     }, [])
 
     return <div>{JSON.stringify(state)}</div>
@@ -31,6 +55,11 @@ export const DeleteTodolist = () => {
 export const UpdateTodolistTitle = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
+        axios.put("https://social-network.samuraijs.com/api/1.1/todo-lists/abd96315-58cc-42a7-a904-226e13c834ce",{ title: "yo"}, settings)
+            .then((res)=>{
+
+                setState(res.data)
+            })
     }, [])
 
     return <div>{JSON.stringify(state)}</div>
