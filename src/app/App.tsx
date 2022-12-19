2ef8b552eb91990594edb5ad927b1b8b2130a6fc
karolinasmile@mail.ru <karolinasmile@mail.ru>
2022-12-19 14:49:28 +0300

    16lesson

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react'
 import './App.css'
-import { TodolistsList } from '../features/TodolistsList/TodolistsList'
-
-// You can learn about the difference by reading this guide on minimizing bundle size.
-// https://mui.com/guides/minimizing-bundle-size/
-// import { AppBar, Button, Container, IconButton, Toolbar, Typography } from '@mui/material';
+import {TodolistsList} from '../features/TodolistsList/TodolistsList'
+import {useAppSelector} from './store'
+import {RequestStatusType} from './app-reducer'
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
-import { Menu } from '@mui/icons-material';
-import LinearProgress from "@mui/material/LinearProgress";
-import {useAppSelector} from "./store";
-import {ErrorSnackbar} from "../components/SnackBar/ErrorSnackbar";
+import LinearProgress from '@mui/material/LinearProgress';
+import {Menu} from '@mui/icons-material';
+import {ErrorSnackbar} from '../components/ErrorSnackbar/ErrorSnackbar'
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
+import {Switch} from "@mui/material";
+import {Login} from "../features/Login";
 
 
 function App() {
-    const status = useAppSelector((state)=>state.app.status)
-
+    const status = useAppSelector<RequestStatusType>((state) => state.app.status)
     return (
+        <BrowserRouter>
         <div className="App">
             <ErrorSnackbar/>
             <AppBar position="static">
@@ -33,12 +33,21 @@ function App() {
                     </Typography>
                     <Button color="inherit">Login</Button>
                 </Toolbar>
+                {status === 'loading' && <LinearProgress/>}
             </AppBar>
-            {status === 'loading' && <LinearProgress color="inherit" />}
             <Container fixed>
-                <TodolistsList/>
+                <Routes>
+                    <Route path='/' element={<TodolistsList />}/>
+                    <Route path='/login' element={<Login/>}/>
+                    <Route path='/404' element={<h1>404:PAGE NOT FOUND</h1>}/>
+                    <Route path='*' element={<Navigate to='/404'/>}/>
+                </Routes>
+
+
+
+
             </Container>
-        </div>
+        </div></BrowserRouter>
     )
 }
 
